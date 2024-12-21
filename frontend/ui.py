6161ad4bea65d7a9989b5487bd4f6648af59413a
LazySlav <72216859+LazySlav@users.noreply.github.com>
2024-12-21 17:09:04 +0300

    ref: ui.py fully renamed according to PEP8
    **General:**
    mixedCase methods -> snake_case
    Ui_classname -> classname
    class atributenames -> [Qt_type]_attributename
    **Specific:**
    BrayleConvertor -> QMainWindow
    Ui_BrayleConvertor -> MainWindow
    centralwidget -> central_widget
    bw_version_change -> btn_toggle_view
    help_opener -> btn_open_help
    select_file_action -> select_file

--- frontend/ui.py
+++ frontend/ui.py
@@ -5,11 +5,11 @@ import logging
 
 logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
-class Ui_HelpWindow(object):
+class HelpWindow(object):
     def __init__(self, html_content):
         self.html_content = html_content
 
-    def setupUi(self, HelpWindow):
+    def setup_ui(self, HelpWindow):
         HelpWindow.setObjectName("HelpWindow")
         HelpWindow.resize(400, 420)
 
@@ -25,10 +25,10 @@ class Ui_HelpWindow(object):
 
         layout.addWidget(self.label)  # Добавляем виджет в layout
 
-        self.retranslateUi(HelpWindow)
+        self.retranslate_ui(HelpWindow)
         QtCore.QMetaObject.connectSlotsByName(HelpWindow)
 
-    def retranslateUi(self, HelpWindow):
+    def retranslate_ui(self, HelpWindow):
         _translate = QtCore.QCoreApplication.translate
         HelpWindow.setWindowTitle(_translate("HelpWindow", "Справка"))
         self.label.setText(_translate("HelpWindow", self.html_content))
@@ -37,40 +37,40 @@ def load_html_content(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
-class Ui_BrayleConvertor(object):
-    def setupUi(self, BrayleConvertor):
-        BrayleConvertor.setObjectName("BrayleConvertor")
-        BrayleConvertor.setEnabled(True)
-        BrayleConvertor.resize(720, 488)
+class MainWindow():
+    def setup_ui(self, QMainWindow : QtWidgets.QMainWindow):
+        QMainWindow.setObjectName("QMainWindow")
+        QMainWindow.setEnabled(True)
+        QMainWindow.resize(720, 488)
 
-        self.centralwidget = QtWidgets.QWidget(parent=BrayleConvertor)
-        self.centralwidget.setMinimumSize(QtCore.QSize(0, 470))
-        self.centralwidget.setObjectName("centralwidget")
+        self.central_widget = QtWidgets.QWidget(parent=QMainWindow)
+        self.central_widget.setMinimumSize(QtCore.QSize(0, 470))
+        self.central_widget.setObjectName("central_widget")
 
-        self.select_file = QtWidgets.QPushButton(parent=self.centralwidget)
-        self.select_file.setGeometry(QtCore.QRect(4, 4, 216, 48))
+        self.btn_select_file = QtWidgets.QPushButton(parent=self.central_widget)
+        self.btn_select_file.setGeometry(QtCore.QRect(4, 4, 216, 48))
         font = QtGui.QFont()
         font.setPointSize(12)
-        self.select_file.setFont(font)
-        self.select_file.setObjectName("select_file")
-        self.select_file.clicked.connect(self.select_file_action)
+        self.btn_select_file.setFont(font)
+        self.btn_select_file.setObjectName("btn_select_file")
+        self.btn_select_file.clicked.connect(self.select_file)
 
-        self.bw_version_change = QtWidgets.QPushButton(parent=self.centralwidget)
-        self.bw_version_change.setGeometry(QtCore.QRect(224, 4, 216, 48))
+        self.btn_toggle_view = QtWidgets.QPushButton(parent=self.central_widget)
+        self.btn_toggle_view.setGeometry(QtCore.QRect(224, 4, 216, 48))
         font = QtGui.QFont()
         font.setPointSize(12)
-        self.bw_version_change.setFont(font)
-        self.bw_version_change.setObjectName("bw_version_change")
+        self.btn_toggle_view.setFont(font)
+        self.btn_toggle_view.setObjectName("btn_toggle_view")
 
-        self.help_opener = QtWidgets.QPushButton(parent=self.centralwidget)
-        self.help_opener.setGeometry(QtCore.QRect(588, 4, 128, 48))
+        self.btn_open_help = QtWidgets.QPushButton(parent=self.central_widget)
+        self.btn_open_help.setGeometry(QtCore.QRect(588, 4, 128, 48))
         font = QtGui.QFont()
         font.setPointSize(12)
-        self.help_opener.setFont(font)
-        self.help_opener.setObjectName("help_opener")
-        self.help_opener.clicked.connect(self.open_help)
+        self.btn_open_help.setFont(font)
+        self.btn_open_help.setObjectName("btn_open_help")
+        self.btn_open_help.clicked.connect(self.open_help)
 
-        self.label_before_select = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label_before_select = QtWidgets.QLabel(parent=self.central_widget)
         self.label_before_select.setGeometry(QtCore.QRect(220, 120, 280, 40))
         font = QtGui.QFont()
         font.setPointSize(12)
@@ -78,7 +78,7 @@ class Ui_BrayleConvertor(object):
         self.label_before_select.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         self.label_before_select.setObjectName("label_before_select")
 
-        self.convert = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.convert = QtWidgets.QPushButton(parent=self.central_widget)
         self.convert.setGeometry(QtCore.QRect(240, 240, 240, 80))
         font = QtGui.QFont()
         font.setPointSize(14)
@@ -86,31 +86,31 @@ class Ui_BrayleConvertor(object):
         self.convert.setDefault(False)
         self.convert.setObjectName("convert")
 
-        BrayleConvertor.setCentralWidget(self.centralwidget)
+        QMainWindow.setCentralWidget(self.central_widget)
 
-        self.retranslateUi(BrayleConvertor)
+        self.retranslate_ui(QMainWindow)
 
-        QtCore.QMetaObject.connectSlotsByName(BrayleConvertor)
+        QtCore.QMetaObject.connectSlotsByName(QMainWindow)
 
-    def retranslateUi(self, BrayleConvertor):
+    def retranslate_ui(self, QMainWindow : QtWidgets.QMainWindow):
         _translate = QtCore.QCoreApplication.translate
-        BrayleConvertor.setWindowTitle(_translate("BrayleConvertor", "BrayleConvertor"))
-        self.select_file.setText(_translate("BrayleConvertor", "Выбрать файл"))
-        self.bw_version_change.setText(_translate("BrayleConvertor", "Версия для слабовидящих"))
-        self.help_opener.setText(_translate("BrayleConvertor", "Справка"))
-        self.label_before_select.setText(_translate("BrayleConvertor", "Выберите файл для конвертации"))
-        self.convert.setText(_translate("BrayleConvertor", "Конвертировать"))
+        QMainWindow.setWindowTitle(_translate("QMainWindow", "QMainWindow"))
+        self.btn_select_file.setText(_translate("QMainWindow", "Выбрать файл"))
+        self.btn_toggle_view.setText(_translate("QMainWindow", "Версия для слабовидящих"))
+        self.btn_open_help.setText(_translate("QMainWindow", "Справка"))
+        self.label_before_select.setText(_translate("QMainWindow", "Выберите файл для конвертации"))
+        self.convert.setText(_translate("QMainWindow", "Конвертировать"))
 
     def open_help(self):
-        logging.debug('ui.Ui_BrayleConvertor.open_help() entered')
+        logging.debug('ui.MainWindow.open_help() entered')
         self.dialog = QtWidgets.QDialog()
         html_file_path = os.path.join(os.path.dirname(__file__), 'help_content.html')
         html_content = load_html_content(html_file_path)
-        self.ui_help = Ui_HelpWindow(html_content)
-        self.ui_help.setupUi(self.dialog)
+        self.ui_help = HelpWindow(html_content)
+        self.ui_help.setup_ui(self.dialog)
         self.dialog.exec()
 
-    def select_file_action(self):
+    def select_file(self):
         # Открытие диалога выбора файла
         file_path, _ = QtWidgets.QFileDialog.getOpenFileName(
             None, "Выберите файл", "", "Все файлы (*)"
@@ -122,8 +122,8 @@ class Ui_BrayleConvertor(object):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    BrayleConvertor = QtWidgets.QMainWindow()
-    ui = Ui_BrayleConvertor()
-    ui.setupUi(BrayleConvertor)
-    BrayleConvertor.show()
+    qmw = QtWidgets.QMainWindow()
+    ui = MainWindow()
+    ui.setup_ui(qmw)
+    qmw.show()
     sys.exit(app.exec())
